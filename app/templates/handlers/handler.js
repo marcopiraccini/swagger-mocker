8cd4641f13c2ddf8f6e8fb373f85c171c904bb1a
Marco Piraccini <marco.piraccini@gmail.com>
2016-02-22 14:24:17 +0100

    Added comment on params

--- app/templates/handlers/handler.js
+++ app/templates/handlers/handler.js
@@ -1,12 +1,16 @@
+/**
+ * Use req.params to get the URL params used in routing (such as {petId}).
+ */
+
 module.exports = {
     get: function (req, reply) {
-        return reply("GET");
+      return reply("GET");
     },
     post: function (req, reply) {
       return reply("POST");
     },
     put: function (req, reply) {
-        return reply("PUT");
+      return reply("PUT");
     },
     delete: function (req, reply) {
       return reply("DELETE");
